8476fe78675e4fa515d18b7af8485ce0d462c1f1
samilkorkmaz <samil.korkmaz@gmail.com>
2015-01-31 19:15:40 +0200

    First commit

---
+++ src/savemycheese/Main.java
@@ -0,0 +1,15 @@
+package savemycheese;
+
+/**
+ *
+ * @author Samil Korkmaz
+ * @date January 2015
+ * @license Public Domain
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
