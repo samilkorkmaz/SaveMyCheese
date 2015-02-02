423530732272c26699d46cd59ace70adc7f3be3b
samilkorkmaz <samil.korkmaz@gmail.com>
2015-02-02 16:57:34 +0200

    Cosmetics

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,6 +1,10 @@
 package view;
 
+import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,13 +22,14 @@ import javax.swing.JPanel;
  */
 public class GameView extends JPanel {
 
-    private static final int PREF_WIDTH = 400;
-    private static final int PREF_HEIGHT = 200;
+    private static final int PREF_WIDTH = 800;
+    private static final int PREF_HEIGHT = 600;
+    private static final int BUTTON_PANEL_WIDTH = 100;
     private static GameView instance;
     private static final JFrame frame = new JFrame("Game - Save My Cheese");
     private static final JButton jbStartPause = new JButton("Pause");
     private static final JButton jbBack = new JButton("Back");
-    private static final JPanel jpCanvas = new JPanel();
+    private static final JPanel jpCanvas = new CanvasPanel();
     private static final JPanel jpButtons = new JPanel();
     private static final java.awt.Font BUTTON_FONT = new java.awt.Font("Tahoma", 1, 12);
 
@@ -42,7 +47,7 @@ public class GameView extends JPanel {
                 frame.setIconImage(ImageIO.read(WelcomeView.ICON));
                 frame.getContentPane().add(instance);
                 frame.pack();
-                frame.setLocationRelativeTo(null);                
+                frame.setLocationRelativeTo(null);
             } catch (IOException ex) {
                 Logger.getLogger(WelcomeView.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -52,25 +57,40 @@ public class GameView extends JPanel {
 
     private GameView() {
         setLayout(null);
-        add(jpCanvas);    
+        add(jpCanvas);
         add(jpButtons);
+
+        jpButtons.setBounds((int) getPreferredSize().getWidth() - (BUTTON_PANEL_WIDTH + 10), 10, BUTTON_PANEL_WIDTH, 50);
+        jpCanvas.setBounds(0, 0, (int)getPreferredSize().getWidth() - (BUTTON_PANEL_WIDTH + 10), (int)getPreferredSize().getHeight());
         
-        
-        jpCanvas.setBounds(0, 0, 200, 100);
-        jpButtons.setBounds(280, 10, 100, 50);
         jpButtons.setLayout(new java.awt.GridLayout(2, 0));
         jpButtons.add(jbStartPause);
         jpButtons.add(jbBack);
-        
+
         jbStartPause.setFont(BUTTON_FONT);
         jbBack.setFont(BUTTON_FONT);
-        
+
         jbStartPause.addActionListener((java.awt.event.ActionEvent evt) -> {
             System.out.println("start");
         });
         
-        jbBack.addActionListener((java.awt.event.ActionEvent evt) -> {
-            frame.setVisible(false);
+        jbBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                makeVisible(false);
+                WelcomeView.makeVisible(true);
+            }
         });
+        
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                WelcomeView.makeVisible(true);
+            }
+        });
+    }
+    
+    public static void makeVisible(boolean isVisible) {
+        frame.setVisible(isVisible);
     }
 }
