1d0bdd370e369867e78d1a1e8d65c3d690a206ef
samilkorkmaz <samil.korkmaz@gmail.com>
2015-02-03 10:58:03 +0200

    Moved MyPolgon calculations from CanvasPanel to GameController

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,14 +1,9 @@
 package controller;
 
-import java.awt.Polygon;
+import java.awt.Point;
 import java.util.List;
-import java.awt.Shape;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import model.MyPolygon;
-import view.GameView;
-import view.WelcomeView;
 
 /**
  * Game controller.
@@ -19,12 +14,58 @@ public class GameController {
 
     private static final List<MyPolygon> polygonList = new ArrayList<>();
     private static final List<MyPolygon> snapPolygonList = new ArrayList<>();
+    private static MyPolygon selectedPolygon = null;
+    private static MyPolygon selectedSnapPolygon = null;
+    private static int prevMouseX;
+    private static int prevMouseY;
 
-    public static List<MyPolygon> getSnapPolygonList() {
+    public static void toggleSelectedShape() {
+        if (selectedPolygon != null) {
+            selectedPolygon = null;
+        }
+    }
+
+    public static void setSelectedShape(int mouseX, int mouseY) {
+        if (GameController.getShapeList().size() > 0) {
+            for (int i = GameController.getShapeList().size() - 1; i >= 0; i--) {
+                if (GameController.getShapeList().get(i).contains(new Point(mouseX, mouseY))) { //if there is a polygon at clicked point
+                    selectedPolygon = GameController.getShapeList().get(i);
+
+                    //move the selected polygon to the end of list so that it will be drawn last (i.e. on top) in paintComponent and checked first for mouse click:
+                    GameController.getShapeList().remove(selectedPolygon);
+                    GameController.getShapeList().add(GameController.getShapeList().size(), selectedPolygon);
+
+                    selectedSnapPolygon = GameController.getSnapShapeList().get(i);
+                    GameController.getSnapShapeList().remove(selectedSnapPolygon);
+                    GameController.getSnapShapeList().add(GameController.getSnapShapeList().size(), selectedSnapPolygon);
+
+                    prevMouseX = mouseX;
+                    prevMouseY = mouseY;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void moveShape(int mouseX, int mouseY) {
+        if (selectedPolygon != null && !selectedPolygon.isSnapped()) {
+            selectedPolygon.translate(mouseX - prevMouseX, mouseY - prevMouseY);
+            prevMouseX = mouseX;
+            prevMouseY = mouseY;
+            selectedPolygon.setIsSnapped(selectedPolygon.isCloseTo(selectedSnapPolygon));
+
+        } else {
+            selectedPolygon = null;
+            selectedSnapPolygon = null;
+        }
+    }
+
+    public static List<MyPolygon> getSnapShapeList() {
         return snapPolygonList;
     }
 
-    public static List<MyPolygon> getPolygonList() {
+    public static List<MyPolygon> getShapeList() {
         return polygonList;
     }
 
