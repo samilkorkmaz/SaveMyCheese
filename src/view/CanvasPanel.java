f3622283fa04bb650026f2bd990eb142a2a8c01e
samilkorkmaz <samil.korkmaz@gmail.com>
2015-02-03 10:58:11 +0200

    Moved MyPolgon calculations from CanvasPanel to GameController

--- src/view/CanvasPanel.java
+++ src/view/CanvasPanel.java
@@ -5,13 +5,11 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Polygon;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
-import model.MyPolygon;
 
 /**
  *
@@ -25,21 +23,19 @@ public class CanvasPanel extends JPanel {
     private static final Color SHAPE_FILL_COLOR = Color.BLUE;
     private static final Color SNAP_SHAPE_LINE_COLOR = Color.BLACK;
     private static final Color SNAP_SHAPE_FILL_COLOR = Color.WHITE;
-    private int prevMouseX;
-    private int prevMouseY;
-
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        for (Shape snapShape : GameController.getSnapPolygonList()) {
+        for (Shape snapShape : GameController.getSnapShapeList()) {
             g2.setColor(SNAP_SHAPE_FILL_COLOR);
             g2.fill(snapShape);
             g2.setStroke(SNAP_SHAPE_STROKE);
             g2.setColor(SNAP_SHAPE_LINE_COLOR);
             g2.draw(snapShape);
         }
-        for (Shape shape : GameController.getPolygonList()) {
+        for (Shape shape : GameController.getShapeList()) {
             g2.setColor(SHAPE_FILL_COLOR);
             g2.fill(shape);
             g2.setStroke(SHAPE_STROKE);
@@ -57,48 +53,18 @@ public class CanvasPanel extends JPanel {
 
     private class MyMouseAdapter extends MouseAdapter {
 
-        private MyPolygon selectedPolygon = null;
-        private MyPolygon selectedSnapPolygon = null;
-
         @Override
         public void mousePressed(MouseEvent evt) {
             if (evt.getButton() == MouseEvent.BUTTON1) {
-                if (GameController.getPolygonList().size() > 0) {
-                    for (int i = GameController.getPolygonList().size() - 1; i >= 0; i--) {
-                        if (GameController.getPolygonList().get(i).contains(evt.getPoint())) { //if there is a polygon at clicked point
-                            selectedPolygon = GameController.getPolygonList().get(i);
-
-                            //move the selected polygon to the end of list so that it will be drawn last (i.e. on top) in paintComponent and checked first for mouse click:
-                            GameController.getPolygonList().remove(selectedPolygon);
-                            GameController.getPolygonList().add(GameController.getPolygonList().size(), selectedPolygon);
-
-                            selectedSnapPolygon = GameController.getSnapPolygonList().get(i);
-                            GameController.getSnapPolygonList().remove(selectedSnapPolygon);
-                            GameController.getSnapPolygonList().add(GameController.getSnapPolygonList().size(), selectedSnapPolygon);
-
-                            prevMouseX = evt.getX();
-                            prevMouseY = evt.getY();
-                            repaint();
-                            break;
-                        }
-                    }
-                }
+                GameController.setSelectedShape(evt.getX(), evt.getY());
+                repaint();
             }
         }
 
         @Override
         public void mouseDragged(MouseEvent evt) {
-            if (selectedPolygon != null) {
-                if (mouseIsInCanvas(evt) && !selectedPolygon.isSnapped()) {
-                    selectedPolygon.translate(evt.getX() - prevMouseX, evt.getY() - prevMouseY);
-                    prevMouseX = evt.getX();
-                    prevMouseY = evt.getY();
-                    selectedPolygon.setIsSnapped(selectedPolygon.isCloseTo(selectedSnapPolygon));
-
-                } else {
-                    selectedPolygon = null;
-                    selectedSnapPolygon = null;
-                }
+            if (mouseIsInCanvas(evt)) {
+                GameController.moveShape(evt.getX(), evt.getY());
             }
             repaint();
 
@@ -106,10 +72,8 @@ public class CanvasPanel extends JPanel {
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            if (selectedPolygon != null) {
-                repaint();
-                selectedPolygon = null;
-            }
+            GameController.toggleSelectedShape();
+            repaint();
         }
     }
 
