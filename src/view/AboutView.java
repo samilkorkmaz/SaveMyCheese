761527ba16a1ed227045d489266fbb86ba6ddfa3
samilkorkmaz <samil.korkmaz@gmail.com>
2015-02-04 22:04:53 +0200

    Added blog link button

--- src/view/AboutView.java
+++ src/view/AboutView.java
@@ -1,8 +1,13 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -25,11 +30,13 @@ public class AboutView extends JPanel {
     private static AboutView instance;
     private static final JFrame frame = new JFrame("About - Save My Cheese");
     private static final JTextArea jtaInfo = new JTextArea("Save My Cheese version 1.0."
-            + "\nProgrammed by Samil Korkmaz, February 2015"
-            + "\nhttp://samilkorkmaz.blogspot.com/");
+            + "\nAuthor: Samil Korkmaz\nDate: February 2015");
+    private static final String BLOG_URL = "http://samilkorkmaz.blogspot.com";
     private static final JButton jbBack = new JButton("Back");
+    private static final JButton jbBlog = new JButton(BLOG_URL);
     private static final JPanel jpButtons = new JPanel();
     private static final java.awt.Font BUTTON_FONT = new java.awt.Font("Tahoma", 1, 12);
+    private URI uri;
 
     @Override
     public Dimension getPreferredSize() {
@@ -53,11 +60,41 @@ public class AboutView extends JPanel {
         frame.setVisible(true);
     }
 
-    private AboutView() {
+    /**
+     * Reference: http://stackoverflow.com/questions/527719/how-to-add-hyperlink-in-jlabel
+     * @param uri 
+     */
+    private static void open(URI uri) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(uri);
+            } catch (IOException e) { /* TODO: error handling */ }
+        } else { /* TODO: error handling */ }
+    }
+
+    private AboutView() {        
+        try {
+            uri = new URI(BLOG_URL);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AboutView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        class OpenUrlAction implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.out.println("Open url " + uri);
+                open(uri);
+            }
+        }
+        jbBlog.setText("Go to Samil's blog...");
+        jbBlog.setToolTipText(uri.toString());
+        jbBlog.addActionListener(new OpenUrlAction());
+        
         setLayout(new java.awt.GridLayout(2, 0));
         add(jtaInfo);
         add(jpButtons);
         jpButtons.setBackground(Color.WHITE);
+        jpButtons.add(jbBlog);
         jpButtons.add(jbBack);
 
         jtaInfo.setEditable(false);
@@ -68,7 +105,7 @@ public class AboutView extends JPanel {
             makeVisible(false);
             WelcomeView.makeVisible(true);
         });
-        
+
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent evt) {
@@ -76,7 +113,7 @@ public class AboutView extends JPanel {
             }
         });
     }
-    
+
     public static void makeVisible(boolean isVisible) {
         frame.setVisible(isVisible);
     }
